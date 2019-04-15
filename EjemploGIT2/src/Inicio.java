99834e2dddfa0fd84969aac63adb796b723d8811
Ana <teaproa@gmail.com>
2019-04-15 08:58:15 +0200

    Ver. egit2

--- EjemploGIT2/src/Inicio.java
+++ EjemploGIT2/src/Inicio.java
@@ -4,6 +4,7 @@ public class Inicio {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola Egit1");
+		System.out.println("hola Egit2");
 	}
 
 }
